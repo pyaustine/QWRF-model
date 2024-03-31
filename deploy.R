a3ca7fc2a3035f2e441ef0417026bb8231a72cc1
sphinx-austin <austinjuniour4@gmail.com>
2024-03-31 10:26:46 +0300

    update deploy logic

---
+++ deploy.R
@@ -0,0 +1,9 @@
+library(rsconnect)
+
+rsconnect::setAccountInfo(name='ywwq7e-austine',
+                          token='7691D476F8D1E50D3B4E3BCCBBB955BE',
+                          secret='GLDPkJWuBojdMma9SATsQM8kFkx2u+UQV9GpEOWi')
+
+rsconnect::deployApp('C:/deploy')
+
+
